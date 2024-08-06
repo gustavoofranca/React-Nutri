4be5f3e8b026ac3c295cc4c72a1b7923c14b76c5
gustavoofranca <engs-gustavofranca@camporeal.edu.br>
2024-08-05 22:14:04 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect , useState} from 'react';
+
+import './App.css'
+
+function App(){
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(()=>{
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+
+    fetch(url)
+    .then((r)=>r.json())
+    .then((json) => setNutri(json))
+  }, []);
+  return(
+    <div>
+      <header className='header'>
+        <strong>React Nutri</strong>
+      </header>
+      {nutri.map((item)=>{
+        return(
+          <article className='artigo' key={item.id}>
+            <strong>{item.titulo}</strong>
+            <img src={item.capa} alt={item.titulo}/>
+            <p>{item.subtitulo}</p>
+            <button>Acessar</button>
+          </article>
+        );
+      })}
+    </div>
+  );
+
+
+}
+
+export default App;
